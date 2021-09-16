d748ed117f2a2602d9923a9a3aa17e7a815d05b3
Maxim Karimov <Juk3z1973@gmail.com>
2021-09-17 02:54:04 +0500

    done

--- contactsOperations/contacts.js
+++ contactsOperations/contacts.js
@@ -18,11 +18,13 @@ function getContactById(contactId) {
         const contactsList = await fs.readFile(contactsPath, 'utf-8');
         const result = JSON.parse(contactsList);
         const findContact = result.find(item => item.id === contactId);
-        console.log(findContact)
-
+        
         if (!findContact) {
-            console.log(`this id doesn't exist`)
-        }
+            console.log(`this id doesn't exist`);
+            return
+        };
+        
+        console.log(findContact)
     };
 
     fetchbyId(contactId);
@@ -32,6 +34,11 @@ function removeContact(contactId) {
     async function fetchbyId(contactId) {
         const contactsList = await fs.readFile(contactsPath, 'utf-8');
         const result = JSON.parse(contactsList);
+
+        if (!result.find(item => item.id === contactId)) {
+            console.log(`this id doesn't exist`);
+            return
+        }
 
         const newList = result.filter(item => item.id !== contactId);
         await fs.writeFile(contactsPath, JSON.stringify(newList, null, 2), 'utf-8');
